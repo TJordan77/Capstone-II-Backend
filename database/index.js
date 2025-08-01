ff855140c772478a7757efff6a9dcbf59d5c6727
Bar Yaakov <baryaakov5050@gmail.com>
2025-08-01 16:48:37 -0400

    added some database stuff (BROKEN)

--- database/index.js
+++ database/index.js
@@ -1,7 +1,42 @@
-const db = require("./db");
-const User = require("./user");
+const sequelize = require("./db");
 
-module.exports = {
-  db,
-  User,
-};
+const User = require("./user");
+const Hunt = require("./hunt");
+
+// Associations for User and Hunt models
+User.hasMany(Hunt, { foreignKey: "creator_id" });
+Hunt.belongsTo(User, { foreignKey: "creator_id" });
+
+// many users can participate in many hunts
+User.belongsToMany(Hunt, {
+  through: "UserHunts",
+  foreignKey: "user_id",
+  otherKey: "hunt_id",
+});
+
+Hunt.belongsToMany(User, {
+  through: "UserHunts",
+  foreignKey: "hunt_id",
+  otherKey: "user_id",
+});
+
+Hunt.belongsTo(Hunt, {
+  as: "OriginalHunt",
+  foreignKey: "original_hunt_id",
+});
+
+Hunt.hasMany(Hunt, {
+  as: "Versions",
+  foreignKey: "original_hunt_id",
+});
+
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("✅ Tables synced");
+    process.exit();
+  })
+  .catch((err) => {
+    console.error("❌ Sync error:", err);
+    process.exit(1);
+  });
