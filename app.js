b3153a889a7221e7982204e30204ac234dfb7448
TJordan77 <tvon777@gmail.com>
2025-08-12 23:21:33 -0400

    Update app.js - vercel connection

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use((req, res, next) => {
     res.setHeader("Vary", "Origin");
   }
   if (req.method === "OPTIONS") {
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, Authorization");
+    res.setHeader("Access--Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, Authorization, X-Requested-With");
     return res.sendStatus(204);
   }
   next();
