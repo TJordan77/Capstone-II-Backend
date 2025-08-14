23b2ce30807c7362b8487e27a734d4af63159e4d
Smedly Moise <smedly.moise@gmail.com>
2025-08-14 05:31:17 -0400

    created leaderboard folder

--- auth/index.js
+++ auth/index.js
@@ -373,39 +373,4 @@ router.get("/me", (req, res) => {
   });
 });
 
-// Leaderboard route
-router.get("/hunts/:huntID/leaderboard",(req, res) => {
-  const {huntID} = req.params;
-
-const mockLeaderBoardData = [
-  {
-    id: 1,
-    completionTime: 120,
-    completionDate: "2025-08-10T12:00:00.000Z",
-    User: {
-      username: 'PlayerOne'
-    }
-  },
-  {
-    id: 2,
-    completionTime: 150,
-    completionDate: "2025-08-09T13:30:00.000Z",
-    User: {
-      username: 'PlayerTwo'
-    }
-  },
-  {
-    id: 3,
-    completionTime: 180,
-    completionDate: "2025-08-11T14:15:00.000Z",
-    User: {
-      username: 'PlayerThree'
-    }
-  },
-];
-
-res.json(mockLeaderBoardData);
-
-
-});
 module.exports = { router, authenticateJWT };
