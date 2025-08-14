b1347cd95ab704b6d40e9dd11285a48964c3b499
TJordan77 <tvon777@gmail.com>
2025-08-14 13:46:30 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,8 +5,8 @@ const huntsRouter = require("./hunts");
 const playRouter = require("./play");
 
 router.use("/test-db", testDbRouter);
-router.use("/", require("./play")); // /api/play/checkpoints/:checkpointId/attempt
-router.use("/hunts", require("./hunts")); 
+router.use("/hunts", huntsRouter); // /api/play/checkpoints/:checkpointId/attempt
+router.use("/play", playRouter);
 
 
 module.exports = router;
