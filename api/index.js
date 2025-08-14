a8b1f1032584b57eed9cb1ae60825809ebefa997
TJordan77 <tvon777@gmail.com>
2025-08-14 13:45:42 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const testDbRouter = require("./test-db");
+const huntsRouter = require("./hunts");
+const playRouter = require("./play");
 
 router.use("/test-db", testDbRouter);
 router.use("/", require("./play")); // /api/play/checkpoints/:checkpointId/attempt
