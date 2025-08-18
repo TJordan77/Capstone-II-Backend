054addc5fe8415af45914cd98fb560cebac1fd5f
TJordan77 <tvon777@gmail.com>
2025-08-18 17:12:24 -0400

    Updated index.js with checkpoints router

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const testDbRouter = require("./test-db");
 const huntsRouter = require("./hunts");
 const playRouter = require("./play");
 const leaderboardRouter = require("./leaderboard");
+const checkpointsRouter = require("./checkpoints");
 
 router.use("/test-db", testDbRouter);
 router.use("/hunts", huntsRouter); // /api/play/checkpoints/:checkpointId/attempt
 router.use("/play", playRouter);
 router.use("/leaderboard", leaderboardRouter); // /api/leaderboard/:huntId
+router.use("/checkpoints", checkpointsRouter); 
 
 module.exports = router;
