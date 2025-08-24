9efbd437890bf10f97dec418b9289b4e896ebd81
TJordan77 <tvon777@gmail.com>
2025-08-24 05:23:04 -0400

    Update userBadge.js

--- database/userBadge.js
+++ database/userBadge.js
@@ -7,7 +7,7 @@ const UserBadge = sequelize.define(
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     userId: { type: DataTypes.INTEGER, allowNull: false, field: "user_id" },
     badgeId: { type: DataTypes.INTEGER, allowNull: false, field: "badge_id" },
-    earnedAt: { type: DataTypes.DATE, field: "earned_at" },
+    earnedAt: { type: DataTypes.DATE, field: "earned_at", defaultValue: DataTypes.NOW },
   },
   {
     tableName: "user_badges",
